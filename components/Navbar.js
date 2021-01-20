037d9c0623a4f868fef546275741c7805be0a3c0
Cristian Castro Astacio <cristiancastroastacio@gmail.com>
2021-01-20 17:41:40 -0400

    css

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import { makeStyles } from '@material-ui/core/styles';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import usuario from '../img/user.png';
+import usuario from '../images/user.png';
 
 import HomeIcon from '@material-ui/icons/Home';
 import SportsCricketIcon from '@material-ui/icons/SportsCricket';
