9639f5f0c092f3eea83c7a0d63c43d565e95fa45
Cristian Castro Astacio <cristiancastroastacio@gmail.com>
2021-02-17 11:59:28 -0400

    hey

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,40 +1,58 @@
 import emailjs from "emailjs-com";
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 
 const ContactForm = () => {
+
+    const [ MensajeAlerta, GuardarMensajeAlerta ] =  useState(false)
 
     function sendEmail(e) {
         e.preventDefault();
 
+        GuardarMensajeAlerta("Enviando");
+
         emailjs.sendForm('service_jqqqg2a', 'template_93l8z2u', e.target, 'user_aBlJT1tOKbd3Gmr5rzvfK')
         .then((result) => {
             console.log(result.text);
+            GuardarMensajeAlerta("Mensaje enviado Exitosamente");
+
+            setTimeout(() => {
+                GuardarMensajeAlerta(false);
+            }, 2000);
+
         }, (error) => {
             console.log(error.text);
+            GuardarMensajeAlerta("Hubo un error");
+
+            setTimeout(() => {
+                GuardarMensajeAlerta(false);
+            }, 2000);
         });
-        e.target.reset()
+        e.target.reset();
     }
 
     return(
         <div>
             <div className="Contenedoe-Send_email">
-                <h3>Déjanos un mensaje:</h3>
-                <form onSubmit={sendEmail}>
-                    <div className="">
-                        <div className="campo-form campo-form-with2">
-                            <input type="text" className="form-control" placeholder="Nombre" name="name"/>
+                {MensajeAlerta ? (<h3 className="alerta-email">{MensajeAlerta}</h3>) : 
+                <Fragment>
+                    <h3>Déjanos un mensaje:</h3>
+                    <form onSubmit={sendEmail}>
+                        <div className="">
+                            <div className="campo-form campo-form-with2">
+                                <input type="text" className="form-control" placeholder="Nombre" name="name"/>
+                            </div>
+                            <div className="campo-form campo-form-with2">
+                                <input type="email" className="form-control" placeholder="Correo" name="email"/>
+                            </div>
+                            <div className="campo-form campo-form-with2">
+                                <textarea className="form-control" id="" cols="30" rows="8" placeholder="Escriba su mensaje" name="message"></textarea>
+                            </div>
+                            <div className="form-group-send">
+                                <input type="submit" className="btn-enviar" value="Enviar Mensaje"></input>
+                            </div>
                         </div>
-                        <div className="campo-form campo-form-with2">
-                            <input type="email" className="form-control" placeholder="Correo" name="email"/>
-                        </div>
-                        <div className="campo-form campo-form-with2">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Escriba su mensaje" name="message"></textarea>
-                        </div>
-                        <div className="form-group-send">
-                            <input type="submit" className="btn-enviar" value="Enviar Mensaje"></input>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </Fragment>}
             </div>
         </div>
     )
